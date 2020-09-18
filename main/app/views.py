b82559abd423a7911a2f063bf71916bb8be09d3d
vildan-valeev <58739557+vildan-valeev@users.noreply.github.com>
2020-09-18 20:14:44 +0500

    create auth decorator for views

--- main/app/views.py
+++ main/app/views.py
@@ -9,38 +9,23 @@ import urllib
 from main.settings import cert, key
 
 
-# def auth(request):
-#     url = "https://slb.medv.ru/api/v2/"
-#     headers = {
-#         'content-type': 'application/json',
-#     }
-#     payload = {
-#         "method": "auth.check",
-#         "params": [],
-#         "jsonrpc": "2.0",
-#         "id": 1,
-#     }
-#     # HttpRequest.POST
-#     response = requests.post(url, cert=(cert, key), verify=False, headers=headers, data=json.dumps(payload))
-#     print(response.json())
-#     # return HttpResponse(f'<h3>{response.json()}</h3>')
-#     return JsonResponse(response.json())
+
+def main(request):
 
 
-
-def auth(request):
-    url = "https://slb.medv.ru/api/v2/"
-    headers = {
-        'content-type': 'application/json',
-    }
-    payload = {
-        "method": "auth.check",
-        "params": [],
-        "jsonrpc": "2.0",
-        "id": 1,
-    }
-    # HttpRequest.POST
-    response = requests.post(url, cert=(cert, key), verify=False, headers=headers, data=json.dumps(payload))
-    print(response.json())
-    # return HttpResponse(f'<h3>{response.json()}</h3>')
     return JsonResponse(response.json())
+    # url = "https://slb.medv.ru/api/v2/"
+    # headers = {
+    #     'content-type': 'application/json',
+    # }
+    # payload = {
+    #     "method": "auth.check",
+    #     "params": [],
+    #     "jsonrpc": "2.0",
+    #     "id": 1,
+    # }
+    # # HttpRequest.POST
+    # response = requests.post(url, cert=(cert, key), verify=False, headers=headers, data=json.dumps(payload))
+    # print(response.json())
+    # # return HttpResponse(f'<h3>{response.json()}</h3>')
+    # return JsonResponse(response.json())

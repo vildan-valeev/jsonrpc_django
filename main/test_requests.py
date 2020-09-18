1d7a9690499c3b56acf1487d19076a3bf0517534
vildan-valeev <58739557+vildan-valeev@users.noreply.github.com>
2020-09-18 14:15:44 +0500

    test urllib request

--- main/test_requests.py
+++ main/test_requests.py
@@ -1,33 +1,32 @@
 import json
-from urllib import request
+import urllib
+
 import requests
 from urllib.parse import urlencode
-from urllib.request import urlopen, Request, URLopener
+from urllib.request import urlopen, Request, URLopener, HTTPSHandler
+import ssl
+
 
 cert = 'client03test.crt'
 key = 'client03test.key'
 url = "https://slb.medv.ru/api/v2/"
-headers = {
-    'content-type': 'application/json',
-}
-values = {
-    "method": "auth.check",
-    "params": [],
-    "jsonrpc": "2.0",
-    "id": 1,
-}
+headers = {'content-type': 'application/json'}
+values = {"method": "auth.check", "params": [], "jsonrpc": "2.0", "id": 1}
 
 
 def main():
+    """requests lib"""
     response = requests.post(url, cert=(cert, key), verify=True, headers=headers, data=json.dumps(values))
     print(response.json())
 
-
 def main2():
-    data = urlencode(values).encode()
-    URLopener(key_file=key, cert_file=cert)
-    # TODO: вписать URLopener в запрос, передать сертификаты в **x509
-    response = urlopen(Request(url=url, data=data, headers=headers, ), timeout=60).read().decode()
+    """urllib"""
+    data = json.dumps(values).encode('utf-8')
+
+    context = ssl.create_default_context()
+    context.load_cert_chain(certfile=cert, keyfile=key)
+    opener = urllib.request.build_opener(urllib.request.HTTPSHandler(context=context))
+    response = opener.open(Request(url=url, data=data, headers=headers, )).read().decode()
     print(response)
 
 
